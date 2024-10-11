323a0a18d875921e1b113449f281aaeddc16b82f
pkzinhogabriel <pkzinhogabriel@gmail.com>
2024-10-11 17:12:37 -0300

    Terminei o Projeto e fiz Build

--- Cobrinha/Assets/Scripts/MenuManager.cs
+++ Cobrinha/Assets/Scripts/MenuManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance; 
 
-    public Snake snake; 
+    public Snake snake;
     public TMP_InputField widthInput;
     public TMP_InputField heightInput;
     public TMP_InputField speedInput;
