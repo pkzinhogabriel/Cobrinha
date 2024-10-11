ae32b4820506de75797e1e3e5755dcafda31fa94
pkzinhogabriel <pkzinhogabriel@gmail.com>
2024-10-11 14:38:33 -0300

    adicionei os text e o score

--- Cobrinha/Assets/Scripts/GameManager.cs
+++ Cobrinha/Assets/Scripts/GameManager.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI HighScoreText;
+    public TextMeshProUGUI gameOverText;
+
+    private int score = 0;
+    private int highScore = 0;
+
     void Start()
     {
-        
+        gameOverText.gameObject.SetActive(false);
+        ScoreText.gameObject.SetActive(true); 
+        HighScoreText.gameObject.SetActive(true); 
+        UpdateScore(0);
+    }
+
+    public void UpdateScore(int points)
+    {
+        score += points;
+        ScoreText.text = "SCORE: " + score.ToString();
+
+        if (score > highScore)
+        {
+            highScore = score;
+            HighScoreText.text = "HIGH SCORE: " + highScore.ToString();
+        }
     }
 
     // Update is called once per frame
